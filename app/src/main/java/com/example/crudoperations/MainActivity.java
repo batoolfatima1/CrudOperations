bc04b0dff343e93f9c0e1afbb0679f8cddcdf7bf
batoolfatima1 <batool12547@gmail.com>
2022-08-20 23:39:58 +0500

    second commit

--- app/src/main/java/com/example/crudoperations/MainActivity.java
+++ app/src/main/java/com/example/crudoperations/MainActivity.java
@@ -27,38 +27,38 @@ public class MainActivity extends AppCompatActivity {
         buttonUpdate = findViewById(R.id.button4);
         buttonDelete= findViewById(R.id.button3);
         buttonAdd = findViewById(R.id.buttonAdd);
-        buttonViewAll = findViewById(R.id.buttonViewAll);
+        buttonViewAll = findViewById(R.id.ViewAll);
         editName = findViewById(R.id.editTextName);
         editRollNumber = findViewById(R.id.editTextRollNumber);
         switchIsActive = findViewById(R.id.switchStudent);
         listViewStudent = findViewById(R.id.listViewStudent);
 
         buttonAdd.setOnClickListener(new View.OnClickListener() {
-            Student studentModel;
+            Student student;
 
             @Override
             public void onClick(View v) {
                 try {
-                    studentModel = new Student(editName.getText().toString(), Integer.parseInt(editRollNumber.getText().toString()), switchIsActive.isChecked());
+                    student = new Student(editName.getText().toString(), Integer.parseInt(editRollNumber.getText().toString()), switchIsActive.isChecked());
                     //Toast.makeText(MainActivity.this, studentModel.toString(), Toast.LENGTH_SHORT).show();
                 }
                 catch (Exception e){
                     Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
                 }
                 dbHelper dbHelper  = new dbHelper(MainActivity.this);
-                dbHelper.addStudent(studentModel);
+                dbHelper.addStudent(student);
 
             }
         });
 
         buttonDelete.setOnClickListener(new View.OnClickListener()
         {
-            Student studentModel;
+            Student student;
 
             @Override
             public void onClick(View v) {
                 try {
-                    studentModel = new Student(editName.getText().toString(), Integer.parseInt(editRollNumber.getText().toString()), switchIsActive.isChecked());
+                    student = new Student(editName.getText().toString(), Integer.parseInt(editRollNumber.getText().toString()), switchIsActive.isChecked());
                     //Toast.makeText(MainActivity.this, studentModel.toString(), Toast.LENGTH_SHORT).show();
                 }
                 catch (Exception e){
@@ -66,17 +66,17 @@ public class MainActivity extends AppCompatActivity {
                 }
                 dbHelper dbHelper  = new dbHelper(MainActivity.this);
 
-                dbHelper.deleteStudent(studentModel);
+                dbHelper.deleteStudent(student);
             }
         });
 
         buttonUpdate.setOnClickListener(new View.OnClickListener() {
-            Student studentModel;
+            Student student;
 
             @Override
             public void onClick(View v) {
                 try {
-                    studentModel = new Student(editName.getText().toString(), Integer.parseInt(editRollNumber.getText().toString()), switchIsActive.isChecked());
+                    student = new Student(editName.getText().toString(), Integer.parseInt(editRollNumber.getText().toString()), switchIsActive.isChecked());
                     //Toast.makeText(MainActivity.this, studentModel.toString(), Toast.LENGTH_SHORT).show();
                 }
                 catch (Exception e){
@@ -84,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 dbHelper dbHelper  = new dbHelper(MainActivity.this);
 
-                dbHelper.updateStudent(studentModel);
+                dbHelper.updateStudent(student);
             }
         });
 
@@ -93,9 +93,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 dbHelper dbHelper = new dbHelper(MainActivity.this);
                 List<Student> list = dbHelper.getAllStudents();
-                ArrayAdapter arrayAdapter = new ArrayAdapter<Student>
+                ArrayAdapter arrayAdapter1 = new ArrayAdapter<Student>
                         (MainActivity.this, android.R.layout.simple_list_item_1,list);
-                listViewStudent.setAdapter(arrayAdapter);
+                listViewStudent.setAdapter(arrayAdapter1);
 
             }
         });
